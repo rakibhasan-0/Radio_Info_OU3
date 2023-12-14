b28ac5c613683b526e5309afc938b6bf6ca81449
Gazi Md Rakibul Hasan <ens20ghn@cs.umu.se>
2023-12-14 12:28:52 +0000

    Update 4 files
    - /src/View/GUI.java
    - /src/View/ProgramView.java
    - /src/View/MenuBarView.java
    - /src/View/ChannelView.java

---
+++ src/View/MenuBarView.java
@@ -0,0 +1,94 @@
+package View;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class MenuBarView {
+    private final JMenuBar menuBar;
+    private final JLabel currentTimeLabel;
+    private final JLabel channelUpdatedLabel;
+    private final JPanel timePanel;
+    private final JLabel selectedChannelLabel;
+    private final JMenu channelTypes;
+    private LocalDateTime lastUpdateTime;
+    private final JLabel programUpdatedLabel;
+
+    public MenuBarView() {
+        menuBar = new JMenuBar();
+        JMenu channel = new JMenu("Channel");
+        JMenu schedule = new JMenu("Schedule");
+        channelTypes = new JMenu("Channel Types");
+
+        JMenuItem updateChannel = new JMenuItem("Update");
+        JMenuItem updateSchedule = new JMenuItem("Update");
+
+        channel.add(updateChannel);
+        schedule.add(updateSchedule);
+        menuBar.add(channel);
+        menuBar.add(schedule);
+        menuBar.add(channelTypes);
+
+        timePanel = new JPanel(new GridLayout(2, 4));
+        currentTimeLabel = new JLabel();
+        programUpdatedLabel = new JLabel("Program updated: ");
+        channelUpdatedLabel = new JLabel("Updating . . . .");
+        timePanel.add(currentTimeLabel);
+        timePanel.add(programUpdatedLabel);
+        timePanel.add(channelUpdatedLabel);
+
+        selectedChannelLabel = new JLabel("Selected Channel: ");
+        timePanel.add(selectedChannelLabel);
+
+    }
+
+    public JMenuBar getMenuBar() {
+        return this.menuBar;
+    }
+
+    public JLabel getChannelUpdatedLabel(){
+        return channelUpdatedLabel;
+    }
+
+
+    public void addUpdateChannelListener(ActionListener listener) {
+        JMenuItem updateChannelListener = menuBar.getMenu(0).getItem(0);
+        updateChannelListener.addActionListener(listener);
+    }
+
+    public void addUpdateScheduleListener(ActionListener listener) {
+        JMenuItem scheduleListener = menuBar.getMenu(1).getItem(0);
+        scheduleListener.addActionListener(listener);
+    }
+
+    public void setSelectedChannelLabel(String channelName) {
+        selectedChannelLabel.setText("Selected Channel: " + channelName);
+    }
+
+    public JLabel getProgramUpdatedLabel(){
+        return programUpdatedLabel;
+    }
+
+    public JPanel getTimePanel() {
+        return timePanel;
+    }
+
+    public void startClock() {
+        Timer clockTimer = new Timer(1000, e -> {
+            LocalDateTime currentTime = LocalDateTime.now();
+            SwingUtilities.invokeLater(() -> setCurrentTimeLabel(currentTime));
+        });
+
+        clockTimer.start();
+    }
+
+    public void setCurrentTimeLabel(LocalDateTime currentTime) {
+        String formattedTime = "Current Time: " + currentTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        SwingUtilities.invokeLater(() -> currentTimeLabel.setText(formattedTime));
+    }
+
+    public JMenu getChannelsTypeMenu() {
+        return channelTypes;
+    }
+}
---
+++ src/View/ProgramView.java
@@ -0,0 +1,46 @@
+package View;
+import javax.swing.*;
+import java.awt.*;
+
+public class ProgramView {
+
+    private JTable programTable;
+    private JScrollPane programScrollPane;
+    private JPanel cardPanel;
+    private JPanel programDetailsPanel;
+
+    public ProgramView() {
+        programTable = new JTable() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return column == 0;
+            }
+        };
+        programTable.setRowHeight(30);
+        programScrollPane = new JScrollPane(programTable);
+
+    }
+
+    public JTable getProgramTable(){
+        return programTable;
+    }
+
+    public JPanel addCardPanel(){
+        cardPanel = new JPanel();
+        programDetailsPanel = new JPanel(new BorderLayout());
+        cardPanel.setLayout(new CardLayout());
+        cardPanel.add("programsList", programScrollPane);
+        cardPanel.add("programDetails",programDetailsPanel);
+        return cardPanel;
+    }
+
+    public JPanel programDetailsPanel(){
+        return programDetailsPanel;
+    }
+    public JPanel getCardPanel(){
+        return cardPanel;
+    }
+    public JScrollPane getProgramScrollPane() {
+        return programScrollPane;
+    }
+}
